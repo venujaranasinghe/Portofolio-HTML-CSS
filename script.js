a9c31553c92128efc88e5a204d60efe954302b75
Venuja Ranasinghe <140576773+venujaranasinghe@users.noreply.github.com>
2024-09-20 15:03:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let typed = new Typed(".multiple-text", {
+    strings: ["Undergraduate", "BCs(hons)Computer Science"],
+    typeSpeed: 100, 
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
